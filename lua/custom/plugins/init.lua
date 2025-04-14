ed21324682b05881d8d6606e237aba715ec075d3
elias <eliashaider95@gmail.com>
2025-04-14 14:29:19 +0200

    add Markdown previewer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,14 +4,33 @@
 -- See the kickstart.nvim README for more information
 return {
 
+  -- AUTOPAIRS () "" '' etc.
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     config = true,
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
   },
-
+  -- MARKDOWN PREVIEW
+  {
+    -- Install markdown preview, use npx if available.
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function(plugin)
+      if vim.fn.executable 'npx' then
+        vim.cmd('!cd ' .. plugin.dir .. ' && cd app && npx --yes npm install')
+      else
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn['mkdp#util#install']()
+      end
+    end,
+    init = function()
+      if vim.fn.executable 'npx' then
+        vim.g.mkdp_filetypes = { 'markdown' }
+      end
+    end,
+  },
+  -- FILE TREE
   {
     'nvim-tree/nvim-tree.lua',
     version = '1.11',
