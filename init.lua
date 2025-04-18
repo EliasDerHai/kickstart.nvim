1f9e778e62ce7b91dbc66d47bad951426084607f
Elias <eliashaider95@gmail.com>
2025-04-18 13:26:48 +0200

    add docker-ls/lsp (not really working though) + replace markdown-preview with markview (markdown-preview is utter trash)

--- init.lua
+++ init.lua
@@ -606,7 +606,8 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        dockerls = {},
+        docker_compose_language_service = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
