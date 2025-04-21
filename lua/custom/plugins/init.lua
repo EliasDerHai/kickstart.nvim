cc9e745af334e4fad52852f95a4b3c667fb99d1c
elias <eliashaider95@gmail.com>
2025-04-21 14:32:24 +0200

    add rustaceanvim and dap setup (incl. dapui, dap-virtual-text & bindings), comment out rust_analyzer (cant conflict with rustaceanvim), minor fix for ocamllsp

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,56 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- Rust advanced
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6',
+    lazy = false,
+    dependencies = { 'mfussenegger/nvim-dap' },
+  },
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
 
+      require('dapui').setup()
+      require('nvim-dap-virtual-text').setup()
+
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<leader>gb', dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set('n', '<leader>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set('n', '<F5>', dap.continue)
+      vim.keymap.set('n', '<F11>', dap.step_into)
+      vim.keymap.set('n', '<F10>', dap.step_over)
+      vim.keymap.set('n', '<F12>', dap.step_out)
+      vim.keymap.set('n', '<F7>', dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
   -- AUTOPAIRS () "" '' etc.
   {
     'windwp/nvim-autopairs',
