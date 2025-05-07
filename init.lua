3613e17b69686b4cb0f1ea80cc4545fa8c986ae8
Elias <eliashaider95@gmail.com>
2025-05-07 22:59:00 +0200

    add GitPullAndReload user command (seems to not work perfectly with nivm tiles/rendering though)

--- init.lua
+++ init.lua
@@ -128,6 +128,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_user_command('GitPullAndReload', function()
+  os.execute 'git pull'
+  vim.cmd 'bufdo! e!'
+  vim.cmd 'redraw!'
+end, {})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
