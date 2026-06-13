1148c835df5e8cae5a54948754faf655dbb29eb3
Elias <eliashaider95@gmail.com>
2026-06-13 13:28:50 +0200

    add harpoon

--- lua/custom/plugins/ui/mini.lua
+++ lua/custom/plugins/ui/mini.lua
@@ -5,6 +5,16 @@ return {
     local statusline = require 'mini.statusline'
     statusline.setup { use_icons = vim.g.have_nerd_font }
     statusline.section_location = function()
+      local ok, harpoon = pcall(require, 'harpoon')
+      if ok then
+        local list = harpoon:list()
+        local current = vim.fn.fnamemodify(vim.fn.expand '%:p', ':.')
+        for i, item in ipairs(list.items) do
+          if item.value == current then
+            return string.format('H:%d  %%2l:%%-2v', i)
+          end
+        end
+      end
       return '%2l:%-2v'
     end
   end,
